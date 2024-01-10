e05feefb89c74ced595d24765aa447697ae83ea7
Nabil Ibarissen <nabilibarissen@Nabils-MacBook-Pro.local>
2024-01-09 22:50:53 -0500

    client created

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import React, { useRef, useCallback, useEffect } from 'react';
+import * as tf from '@tensorflow/tfjs';
+import '@tensorflow/tfjs-backend-webgl';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import Webcam from 'react-webcam';
+
+function App() {
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
